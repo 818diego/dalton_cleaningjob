52fb14bead592799e8bd7e64385534e4e0ed3805
Diego <119649331+818diego@users.noreply.github.com>
2024-12-29 16:36:04 -0800

    add coments for config

--- config/config.lua
+++ config/config.lua
@@ -7,49 +7,51 @@ Config.BonusMin = 50    -- Minimum bonus for every 10 cars
 Config.BonusMax = 300   -- Maximum bonus for every 10 cars
 Config.CarsToClean = 10 -- Number of cars for additional bonus
 
+-- Job NPC configuration
 Config.JobNPC = {
-    model = 's_m_y_garbage',                -- NPC model
-    coords = vec3(-348.76, -772.78, 33.97), -- Coordinates
-    heading = 270.0                         -- Heading
+    model = 's_m_y_garbage',               -- NPC model
+    coords = vec3(-826.82, -731.6, 28.05), -- Coordinates
+    heading = 180.0                        -- Heading
 }
 
+-- Job blip configurations
 Config.JobBlip = {
-    enabled = true,        -- Show blip or not
-    sprite = 318,          -- Blip icon (see: https://docs.fivem.net/docs/game-references/blips/)
-    color = 2,             -- Blip color (see: https://docs.fivem.net/docs/game-references/blips/)
-    scale = 0.8,           -- Blip size
-    label = "Car Cleaning" -- Name shown on the map
+    enabled = true,            -- Show blip or not
+    sprite = 100,              -- Blip icon (see: https://docs.fivem.net/docs/game-references/blips/)
+    color = 5,                 -- Blip color (see: https://docs.fivem.net/docs/game-references/blips/)
+    scale = 0.9,               -- Blip size
+    label = "Car Cleaning Job" -- Name shown on the map
 }
 
-Config.VehicleDistance = 3.0 -- Distance to activate interaction
+Config.VehicleDistance = 1.0 -- Distance to activate interaction
 
 Config.Language = 'en'       -- Default language ('en' for English, 'es' for Spanish)
 
-Config.WorkClothes = {
+Config.WorkClothes = {       -- configured clothes for the jobs
     male = {
-        ['tshirt_1'] = 15,
-        ['tshirt_2'] = 0,
-        ['torso_1'] = 65,
-        ['torso_2'] = 0,
-        ['arms'] = 0,
-        ['pants_1'] = 38,
-        ['pants_2'] = 0,
-        ['shoes_1'] = 12,
-        ['shoes_2'] = 0,
-        ['chain_1'] = 0,
-        ['chain_2'] = 0
+        ['tshirt_1'] = 15,   -- T-shirt model
+        ['tshirt_2'] = 0,    -- T-shirt texture/variation
+        ['torso_1'] = 65,    -- Torso model
+        ['torso_2'] = 0,     -- Torso texture/variation
+        ['arms'] = 0,        -- Arms model
+        ['pants_1'] = 38,    -- Pants model
+        ['pants_2'] = 0,     -- Pants texture/variation
+        ['shoes_1'] = 12,    -- Shoes model
+        ['shoes_2'] = 0,     -- Shoes texture/variation
+        ['chain_1'] = 0,     -- Chain/Accessory model
+        ['chain_2'] = 0      -- Chain/Accessory texture/variation
     },
     female = {
-        ['tshirt_1'] = 15,
-        ['tshirt_2'] = 0,
-        ['torso_1'] = 59,
-        ['torso_2'] = 0,
-        ['arms'] = 0,
-        ['pants_1'] = 38,
-        ['pants_2'] = 0,
-        ['shoes_1'] = 12,
-        ['shoes_2'] = 0,
-        ['chain_1'] = 0,
-        ['chain_2'] = 0
+        ['tshirt_1'] = 15, -- T-shirt model
+        ['tshirt_2'] = 0,  -- T-shirt texture/variation
+        ['torso_1'] = 59,  -- Torso model
+        ['torso_2'] = 0,   -- Torso texture/variation
+        ['arms'] = 0,      -- Arms model
+        ['pants_1'] = 38,  -- Pants model
+        ['pants_2'] = 0,   -- Pants texture/variation
+        ['shoes_1'] = 12,  -- Shoes model
+        ['shoes_2'] = 0,   -- Shoes texture/variation
+        ['chain_1'] = 0,   -- Chain/Accessory model
+        ['chain_2'] = 0    -- Chain/Accessory texture/variation
     }
 }
